6df1b3749688fb08bb1bf7b6dfd7d3a91198075b
Tomas <tmorenodelafuente@gmail.com>
2023-05-16 22:55:36 +0200

    a

--- resources/views/components/sections/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <span class="fa fa-fw fa-home fa-2x selected_dashboard icon-white"></span>
-    @if (!isset($default))
-    <span class="fa fa-fw fa-cogs fa-2x"></span>
-    @endif
-</footer>
